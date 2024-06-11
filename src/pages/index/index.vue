773786a46fc6e02e87d8b8cae38dbff42d1c0380
zhuhao <1272016961@qq.com>
2024-06-11 17:46:14 +0800

    轮播图以及ts类型请求

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,27 @@
 <script setup lang="ts">
-//
+import { getHomeBannerAPI } from '@/services/home'
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+import type { BannerItem } from '@/types/home'
+import XtxSwiper from '@/components/XtxSwiper.vue'
+import CustomNavBar from './components/CustomNavBar.vue'
+
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const data = await getHomeBannerAPI()
+  console.log(data)
+  bannerList.value = data.result
+}
+
+onLoad(() => {
+  getHomeBannerData()
+})
 </script>
 
 <template>
+  <CustomNavBar />
+  <!-- 自定义轮播图 -->
+  <XtxSwiper :list="bannerList" />
   <view class="index">index</view>
   <uni-section title="双标题卡片" type="line">
     <uni-card
---
+++ src/types/home.d.ts
@@ -0,0 +1,10 @@
+/**
+ * 首页-广告位数据类型
+ */
+
+export type BannerItem = {
+  herfUrl: string
+  id: string
+  imgUrl: string
+  type: number
+}
