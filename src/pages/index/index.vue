e29cd747caefbd72d0efab76d881362407d54915
xukangle <623679990@qq.com>
2024-06-19 21:02:42 +0800

    banner

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -22,17 +22,6 @@ onLoad(() => {
   <CustomNavBar />
   <!-- 自定义轮播图 -->
   <XtxSwiper :list="bannerList" />
-  <view class="index">index</view>
-  <uni-section title="双标题卡片" type="line">
-    <uni-card
-      title="基础卡片"
-      sub-title="副标题"
-      extra="额外信息"
-      thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
-    >
-      <text class="uni-body">这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
-    </uni-card>
-  </uni-section>
 </template>
 
 <style lang="scss">
