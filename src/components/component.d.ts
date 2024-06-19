85559ed212a1307b283a1f6ec81729dfce9fa895
xukangle <623679990@qq.com>
2024-06-20 00:14:34 +0800

    首页列表 开发

--- src/components/component.d.ts
+++ src/components/component.d.ts
@@ -4,8 +4,14 @@
  * declare module 'vue'
  */
 import XtxSwiper from './XtxSwiper.vue'
+import XtGuess from './XtGuess.vue'
+
 declare module 'vue' {
   export interface GlobalComponents {
     XtxSwiper: typeof XtxSwiper
+    XtGuess: typeof XtGuess
   }
 }
+
+// 组件实例类型
+export type XtxSwiperInstance = InstanceType<typeof XtGuess>
--- src/services/home.ts
+++ src/services/home.ts
@@ -1,4 +1,5 @@
-import type { BannerItem } from '@/types/home'
+import type { BannerItem, CategoryItem, HotItem, GuessItem } from '@/types/home'
+import type { PageResult, PageParams } from '@/types/global'
 import { http } from '@/utils/http'
 
 export const getHomeBannerAPI = (distributionSite = 1) => {
@@ -8,5 +9,40 @@ export const getHomeBannerAPI = (distributionSite = 1) => {
     data: {
       distributionSite,
     },
+  })
+}
+
+/**
+ * 获取首页前台类目
+ * home/category
+ */
+export const getHomeCategoryAPI = () => {
+  return http<CategoryItem[]>({
+    url: '/home/category/mutli',
+    method: 'GET',
+  })
+}
+
+/**
+ * 获取首页热门推荐
+ * home/hot
+ *
+ */
+export const getHomeHotAPI = () => {
+  return http<HotItem[]>({
+    url: '/home/hot/mutli',
+    method: 'GET',
+  })
+}
+
+/**
+ * 猜你喜欢
+ * home/guess
+ */
+export const getHomeGoodsGuessLikeAPI = (data?: PageParams) => {
+  return http<PageResult<GuessItem>>({
+    method: 'GET',
+    url: '/home/goods/guessLike',
+    data,
   })
 }
